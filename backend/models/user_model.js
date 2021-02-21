eecaf4cd944a2f38dde3343aaa5c3248011d0478
Will <wbecht1@knights.ucf.edu>
2021-02-21 17:20:34 -0500

    Updated events and user models

--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -1,15 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const RSO = require("../models/rso_model.js");
-const University = require("../models/university_model.js");
+const RSO = require("./rso_model.js");
+const University = require("./university_model.js");
 
 const userSchema = new Schema({
-  username: {type: String, required: true, minlength: 1},
-  password: {type: String, required: true, minlength: 1},
-  type: {type: String},
-  rso: {type: Schema.Types.ObjectId, ref: RSO},
-  university: {type: Schema.Types.ObjectId, ref: University},
+  username: {
+    type: String,
+    required: true,
+    minlength: 1
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 1
+  },
+
+  // Normal, Admin, SuperAdmin
+  type: {
+    type: String
+    default: 'standard'
+  },
+
+  rso: {
+    type: Schema.Types.ObjectId,
+    ref: RSO
+  },
+
+  university: {
+    type: Schema.Types.ObjectId,
+    ref: University
+  },
 });
 
 const User = mongoose.model('User', userSchema);
