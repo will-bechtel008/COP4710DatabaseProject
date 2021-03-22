980b3d417a73cc406d811d010b31396985bb0edc
Will <wbecht1@knights.ucf.edu>
2021-03-22 17:14:11 -0400

    Initial commit to events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import * as React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Login from './auth/Login.js'
-import Events from './events/Events.js'
-import Org from './org/Org.js'
-import { BrowserRouter, Route } from 'react-router-dom';
 
-class App extends React.Component {
-  render(): React.Node {
-    return (
-      <BrowserRouter>
-        <Route path='/' exact component={Login} />
-        <Route path='/Events' exact component={Events} />
-        <Route path='/Org' exact component={Org} />
-      </BrowserRouter>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
