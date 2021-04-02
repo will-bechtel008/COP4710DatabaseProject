3e1c3d16f4c95009f15c60dcadbe4baddc37ba68
Will <wbecht1@knights.ucf.edu>
2021-04-01 21:20:28 -0400

    nothing

--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -7,6 +7,7 @@ const University = require("../models/university_model.js");
 const userSchema = new Schema({
   username: {type: String, required: true, minlength: 1},
   password: {type: String, required: true, minlength: 1},
+  // normal, admin, superadmin
   type: {type: String},
   rso: {type: Schema.Types.ObjectId},
   university: {type: Schema.Types.ObjectId},
